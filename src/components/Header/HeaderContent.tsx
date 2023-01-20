1e1da9816fd546367057e4e2916b3953ac71a3c7
aldrinreal <real_aldrin@yahoo.com>
2023-01-20 11:29:21 +0800

    update title

--- src/components/Header/HeaderContent.tsx
+++ src/components/Header/HeaderContent.tsx
@@ -50,7 +50,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           {/* <img src={} alt="" /> logo here */}
-          <Link to="/">UNREEL</Link>
+          <Link to="/">TFLIX</Link>
         </div>
         <ul className="header__nav">
           {headerNav.map((e, i) => (
